5831c9df91e16f70c93a6ae5ebb903e7946e30cd
Fritz Geib <fritz@standland.org>
2022-09-27 09:53:06 -0400

    port name.sh to api, a bit slower :(

--- name.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TBA_LINK="https://www.thebluealliance.com/team/$1/"
-TEAM_PAGE="$(curl --silent "$TBA_LINK")"
-
-
-echo "$TEAM_PAGE" | grep "<title>" | xargs | sed "s/<title>//" | sed "s/ - .*//"
-
