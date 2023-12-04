235673f8c4fb3e8dc58cf60028e2654dfc002738
Fritz Geib <fritz@standland.org>
2023-12-04 08:18:23 -0500

    blah

--- scanner.py
+++ scanner.py
@@ -1,11 +1,35 @@
-def codeFilter(code: str) -> bool:
-    #code filter function
-    return type(code) == str and code is not None and code != ""
+DATABASE = "./database.csv"
+#database format:
+#Barcode, Stock Level, Image, Name, Category, Description, Quantity, Container
+
+def checkItem(code: str) -> bool:
+    #check if item barcode is in database
+    with open(DATABASE) as f:
+        for line in f:
+            if code in line:
+                return True
+            
+def addItem(code: str, name: str, category: str, description: str, quantity: int, container: str):
+    #add item to database
+    with open(DATABASE, "a") as f:
+        f.write(f"{code}, , , {name}, {category}, {description}, {quantity}, {container}\n")
+    
 
 while True:
     code = input()
     #remaining code will only run when a code is scanned
-    if code == "exit":
+    if code == "exit" or type(code) != str or code is None or code == "": #filter out invalid inputs
         break
-    if codeFilter(code):
-        print(code)
+
+    if checkItem(code):
+        print("Item found!")
+    else:
+        print("Item not found in database")
+        name = input("Enter name: ")
+        category = input("Enter category: ")
+        description = input("Enter description: ")
+        quantity = int(input("Enter quantity: "))
+        container = input("Enter container: ")
+        #add item to database
+        addItem(code, name, category, description, quantity, container)
+        print("Item added to database")
